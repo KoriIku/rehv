0bd5a8abd419e5c355328d91f61d775be09885a7
KoriIku <86508666+KoriIku@users.noreply.github.com>
2022-12-14 21:16:25 +0800

    可以加载热榜目录了
    但是卡片样式还没搞定

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { log } from 'console';
+import * as React from 'react';
+import { useEffect } from 'react';
+import parseTable from "./tools"
+import { List, Descriptions, ButtonGroup, Rating, Button } from '@douyinfe/semi-ui';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+declare global {
+  interface Window {
+    getHelloWorld: any;
+  }
 }
 
-export default App
+function App() {
+  const [result, setResult] = React.useState<any | string | null>(null);
+
+  useEffect(() => {
+    if (typeof window.getHelloWorld === 'function') {
+      console.log(typeof window.getHelloWorld);
+      window.getHelloWorld().then((res: string) => {
+        console.log('已加载插件effect');
+        const rows = parseTable(res);
+        console.log(rows.map((obj: { cover: string }) => obj.cover));
+        setResult(rows);
+      });
+    } else {
+      console.log('没加载插件1');
+      setResult('Hello World from React App');
+    }
+  }, []);
+  return (
+    <div style={{ marginRight: 16 }}>
+    <List
+      bordered
+      dataSource={result}
+      renderItem={item => <List.Item>
+
+            <img src={item.cover} alt={item.title} style={{ width: '100%' }} />
+
+            <h3 style={{ color: 'var(--semi-color-text-0)', fontWeight: 500 }}>{item.title}</h3>
+            <p>{item.author}</p>
+
+                <Rating allowHalf value={item.rating} />
+
+                {item.language}
+
+                {item.category}
+
+                {item.time}
+
+      </List.Item>}
+    />
+  </div>
+  );
+}
+
+export default App;
