bace9ffbece17394ec35b51058f8f0f94f504009
KoriIku <86508666+KoriIku@users.noreply.github.com>
2022-12-17 01:27:50 +0800

    分离画廊列表组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import { log } from 'console';
-import * as React from 'react';
-import { useEffect } from 'react';
-import parseTable from "./tools"
-import { Card, Col, List, Rating, Row } from '@douyinfe/semi-ui';
-import ColorChip from './components/ColorChip';
-import GallaryCard from './components/GallaryCard';
+import GallaryList from './components/GallaryList';
 
 declare global {
   interface Window {
@@ -13,31 +7,9 @@ declare global {
 }
 
 function App() {
-  const [result, setResult] = React.useState<any | string | null>(null);
 
-  useEffect(() => {
-    if (typeof window.getHelloWorld === 'function') {
-      console.log(typeof window.getHelloWorld);
-      window.getHelloWorld().then((res: string) => {
-        console.log('已加载插件effect');
-        const rows = parseTable(res);
-        console.log(rows.map((obj: { time: string }) => obj.time));
-        setResult(rows);
-      });
-    } else {
-      console.log('没加载插件1');
-      setResult('Hello World from React App');
-    }
-  }, []);
   return (
-    <div style={{}}>
-      <List
-        bordered
-        dataSource={result}
-        renderItem={item => <GallaryCard item={item} />
-        }
-      />
-    </div>
+    <GallaryList />
   );
 }
 
---
+++ src/components/GallaryList.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { useEffect } from 'react';
+import parseTable from "../tools"
+import { List } from '@douyinfe/semi-ui';
+import GallaryCard from './GallaryCard';
+
+
+function GallaryList() {
+    const [result, setResult] = React.useState<any | string | null>(null);
+  
+    useEffect(() => {
+      if (typeof window.getHelloWorld === 'function') {
+        console.log(typeof window.getHelloWorld);
+        window.getHelloWorld().then((res: string) => {
+          console.log('已加载插件effect');
+          const rows = parseTable(res);
+          console.log(rows.map((obj: { time: string }) => obj.time));
+          setResult(rows);
+        });
+      } else {
+        console.log('没加载插件1');
+        setResult('Hello World from React GallaryList');
+      }
+    }, []);
+  
+    return (
+
+  
+        <div style={{}}>
+          <List
+            bordered
+            dataSource={result}
+            renderItem={item => <GallaryCard item={item} />
+            }
+          />
+        </div>
+    );
+  }
+  export default GallaryList;
