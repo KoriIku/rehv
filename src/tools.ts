18e0e71126e00b4b8424c94df0f8ba2a9356b5e0
KoriIku <86508666+KoriIku@users.noreply.github.com>
2022-12-22 23:15:33 +0800

    正则匹配前判断一下字符串是否有值

--- src/tools.ts
+++ src/tools.ts
@@ -30,14 +30,17 @@ export function parseGallaryList(html: string) {
         item.uploader = $elem.find('.gl4c div:first-child').text();
 
         // 获取第三列第一个 <a> 标签的 href 属性
-        const href = $elem.find('.gl3c a').first().attr('href')!;
+        const href = $elem.find('.gl3c a').first().attr('href');
 
-        // 使用正则表达式从 href 中解析出 gid 和 gtoken 的值
-        const matches = href.match(/g\/(\d+)\/([\w-]+)/);
-        if (matches) {
-            item.gid = matches[1];
-            item.gtoken = matches[2];
+        if (href !== undefined) {
+            // 使用正则表达式从 href 中解析出 gid 和 gtoken 的值
+            const matches = href!.match(/g\/(\d+)\/([\w-]+)/);
+            if (matches) {
+                item.gid = matches[1];
+                item.gtoken = matches[2];
+            }
         }
+
         items.push(item);
     });
 
@@ -56,9 +59,9 @@ export function parseGallaryDetail(html: string) {
     const element = $('#gdd > table > tbody > tr:nth-child(6) > td.gdt2');
     const text: string = element.text()!;
     const pages = parseInt(text!.match(/\d+/)![0], 10);
-    console.log(pages+' pages');
+    console.log(pages + ' pages');
 
     //获取每次预览的图片数
     const previewPrePage = $('#gdt > div.gdtm').length;
-    console.log(previewPrePage+' pages pre preview');
+    console.log(previewPrePage + ' pages pre preview');
 }
