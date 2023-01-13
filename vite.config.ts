748b7606f3c4f9a80d45c5e7a319dc5cb944a01f
KoriIku <86508666+KoriIku@users.noreply.github.com>
2023-01-14 02:09:54 +0800

    修改vite启动端口和绑定ip，不然wsl上启动没办法再win里用localhost访问

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 13001,
+    // 是否开启 https
+    https: false,
+  }
 })
