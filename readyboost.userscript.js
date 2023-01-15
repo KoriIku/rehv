a7a72d01cbd97cff407b52545bfa50a7e3a61fde
KoriIku <86508666+KoriIku@users.noreply.github.com>
2023-01-15 16:19:48 +0800

    改掉脚本的默认名字

--- readyboost.userscript.js
+++ readyboost.userscript.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         My Greasemonkey Plugin
+// @name         Rehv ReadyBoost
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
